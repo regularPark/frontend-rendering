b362e74601f141aa443566a123cfc74e5102097b
regularPark <srk99279@naver.com>
2023-10-16 15:15:47 +0900

    refactor: SEO 최적화

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
+      <title>Stamp Crush</title>
+      <meta
+        name="description"
+        content="흩어져있는 카페 쿠폰을 한 곳에서 관리하자."
+      />
       <Head />
       <body>
         <Main />
